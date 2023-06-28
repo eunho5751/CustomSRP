9ef3a0d6ce90ff2b1e38e2d98f299ae7bd4fc95d
최은호 <eunho5751@cellbig.com>
2023-06-28 11:52:00 +0900

    Project Initialization

---
+++ Assets/CustomRP/Scripts/CustomRenderPipeline.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CustomRenderPipeline : RenderPipeline
+{
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+    }
+}
