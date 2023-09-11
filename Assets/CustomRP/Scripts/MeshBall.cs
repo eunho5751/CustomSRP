9b597d58d03eb5987587b674b72cbfad8db69d03
LAPTOP-VQJCEHJJ\eunho <eunho5751@naver.com>
2023-09-11 23:00:48 +0900

    Chapter 5. Baked Light - Mesh Ball [ Light Probes | LPPV ]

--- Assets/CustomRP/Scripts/MeshBall.cs
+++ Assets/CustomRP/Scripts/MeshBall.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 using Random = UnityEngine.Random;
 
@@ -13,34 +14,50 @@ public class MeshBall : MonoBehaviour
     private Mesh _mesh;
     [SerializeField]
     private Material _material;
+    [SerializeField]
+    private LightProbeProxyVolume _lightProbeVolume;
 
     private Matrix4x4[] _matrices = new Matrix4x4[1023];
-    private Vector4[] _baseColors = new Vector4[1023];
-    private float[] _metallics = new float[1023];
-    private float[] _smoothnesses = new float[1023];
 
     private MaterialPropertyBlock _block;
 
     private void Awake()
     {
+        _block = new MaterialPropertyBlock();
+
+        var baseColors = new Vector4[1023];
+        var metallics = new float[1023];
+        var smoothnesses = new float[1023];
+        var lightProbes = new SphericalHarmonicsL2[1023];
+
         for (int i = 0; i< _matrices.Length; i++)
         {
             _matrices[i] = Matrix4x4.TRS(Random.insideUnitSphere * 10f, 
                 Quaternion.Euler(Random.value * 360f, Random.value * 360f, Random.value * 360f), 
                 Vector3.one * Random.Range(0.5f, 1.5f));
-            _baseColors[i] = new Vector4(Random.value, Random.value, Random.value, 1f);
-            _metallics[i] = Random.value < 0.25f ? 1f : 0f;
-            _smoothnesses[i] = Random.Range(0.05f, 0.95f);
+            baseColors[i] = new Vector4(Random.value, Random.value, Random.value, 1f);
+            metallics[i] = Random.value < 0.25f ? 1f : 0f;
+            smoothnesses[i] = Random.Range(0.05f, 0.95f);
         }
+        _block.SetVectorArray(_baseColorId, baseColors);
+        _block.SetFloatArray(_metallicId, metallics);
+        _block.SetFloatArray(_smoothnessId, smoothnesses);
 
-        _block = new MaterialPropertyBlock();
-        _block.SetVectorArray(_baseColorId, _baseColors);
-        _block.SetFloatArray(_metallicId, _metallics);
-        _block.SetFloatArray(_smoothnessId, _smoothnesses);
+        if (_lightProbeVolume != null)
+        {
+            var positions = new Vector3[1023];
+            for (int i = 0; i < _matrices.Length; i++)
+            {
+                positions[i] = _matrices[i].GetColumn(3);
+            }
+            LightProbes.CalculateInterpolatedLightAndOcclusionProbes(positions, lightProbes, null);
+            _block.CopySHCoefficientArraysFrom(lightProbes);
+        }
     }
 
     private void Update()
     {
-        Graphics.DrawMeshInstanced(_mesh, 0, _material, _matrices, 1023, _block);
+        Graphics.DrawMeshInstanced(_mesh, 0, _material, _matrices, 1023, _block,
+            ShadowCastingMode.On, true, 0, null, _lightProbeVolume ? LightProbeUsage.UseProxyVolume : LightProbeUsage.CustomProvided, _lightProbeVolume);
     }
 }
